40f39149c18e7c0821cc5331926724d08166adae
astic1764 <astic1764@gmail.com>
2020-03-18 16:47:25 +0900

    Updates

---
+++ precache-manifest.dc965e31d5b6d600470605df0fc7c826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a15df36e1cbf7b69c81d7977517450",
+    "url": "/daily-diary/index.html"
+  },
+  {
+    "revision": "074f6298ed4dd9ebc754",
+    "url": "/daily-diary/static/js/2.a549c77e.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/daily-diary/static/js/2.a549c77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7243f1ee0c224730805c",
+    "url": "/daily-diary/static/js/main.93f1567b.chunk.js"
+  },
+  {
+    "revision": "0bc618416b8e7f1de9cc",
+    "url": "/daily-diary/static/js/runtime-main.d94dc6b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daily-diary/precache-manifest.1f9d623b7232f2d12abceea7243442d1.js"
+  "/daily-diary/precache-manifest.dc965e31d5b6d600470605df0fc7c826.js"
 );
 
 self.addEventListener('message', (event) => {
